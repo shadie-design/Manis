97efea06f97c8cbcc959c1031ce5137054125f61
Shadrack Githua <githuashadrack89@gmail.com>
2023-02-15 12:35:09 +0300

    Test commit

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@ include("functions/functions.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Manis cosmetics</title>
+    <title>Manis cosmetics </title>
     <link rel="stylesheet" href="styles/bootstrap-337.min.css">
     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
